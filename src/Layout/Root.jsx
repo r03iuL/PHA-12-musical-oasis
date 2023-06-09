9db348c0e4c970cf49226398a14c0c14d8654971
r03iuL <robiulislam1806@gmail.com>
2023-06-09 12:32:06 +0600

    Nav Bar Added

--- src/Layout/Root.jsx
+++ src/Layout/Root.jsx
@@ -1,14 +1,13 @@
 import { Outlet } from "react-router-dom";
+import NavBar from "./../Pages/Common/NavBar/NavBar";
 
 const Root = () => {
-    return (
-        <div>
-            <Outlet>
-               <p>hello world</p> 
-            </Outlet>
-            
-        </div>
-    );
+  return (
+    <div>
+      <NavBar></NavBar>
+      <Outlet></Outlet>
+    </div>
+  );
 };
 
 export default Root;

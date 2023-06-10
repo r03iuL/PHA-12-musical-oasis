2ef2394e6fd46b438ab17e269beea8cfe2d557e6
r03iuL <robiulislam1806@gmail.com>
2023-06-10 21:37:54 +0600

    Environmental Variable added

--- src/Firebase/firebase.config.js
+++ src/Firebase/firebase.config.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyC8QDKX08_zO-cQh5YfTzGhMIW5VwCG048",
-    authDomain: "musical-oasis.firebaseapp.com",
-    projectId: "musical-oasis",
-    storageBucket: "musical-oasis.appspot.com",
-    messagingSenderId: "222234954262",
-    appId: "1:222234954262:web:ff3a8a78b94c5aaf3b8337"
+    apiKey:import.meta.env.VITE_apiKey,
+    authDomain:import.meta.env.VITE_authDomain,
+    projectId:import.meta.env.VITE_projectId,
+    storageBucket:import.meta.env.VITE_storageBucket,
+    messagingSenderId:import.meta.env.VITE_messagingSenderId,
+    appId:import.meta.env.VITE_appId
 };
 
 // Initialize Firebase

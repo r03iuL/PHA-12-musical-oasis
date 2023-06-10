ade0bb7529aff4d0d26c6aa8a5e794687fcd9bfc
r03iuL <robiulislam1806@gmail.com>
2023-06-10 23:06:48 +0600

    Popular instructors responsive done

--- src/Pages/Home/PopularInstructors/PopularInstructors.jsx
+++ src/Pages/Home/PopularInstructors/PopularInstructors.jsx
@@ -1,8 +1,8 @@
-import { useState } from "react";
-import { useEffect } from "react";
+import { useState, useEffect } from "react";
 
 const PopularInstructors = () => {
   const [instructors, setInstructors] = useState([]);
+
   useEffect(() => {
     fetch("Instructors.json")
       .then((res) => res.json())
@@ -11,22 +11,22 @@ const PopularInstructors = () => {
 
   return (
     <div>
-     <p className="border-y-4 border-indigo-500 mx-auto text-center p-10 text-3xl font-bold">
+      <p className="border-b-4 border-indigo-500 mx-auto text-center mt-10 p-10 text-3xl font-bold">
         | Popular Instructors |
       </p>
-      <div className="py-10 grid grid-cols-3 gap-10">
-        {instructors.map((instructors, index) => (
-          <div key={index} className="card  bg-base-100 shadow-xl">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10 py-10">
+        {instructors.map((instructor, index) => (
+          <div key={index} className="card bg-base-100 shadow-xl">
             <figure>
               <img
-                src={instructors.Image_link}
-                alt={instructors.Name}
+                src={instructor.Image_link}
+                alt={instructor.Name}
                 className="rounded-full h-3/4"
               />
             </figure>
             <div className="card-body">
-              <h2 className="card-title">{instructors.Name}</h2>
-              <p>Students:{instructors.Student_number}</p>
+              <h2 className="card-title">{instructor.Name}</h2>
+              <p>Students: {instructor.Student_number}</p>
               <div className="card-actions justify-end">
                 <button className="btn btn-primary">Details</button>
               </div>

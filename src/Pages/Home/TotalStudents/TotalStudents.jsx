0f25bd7e5e51a7b1aa1a253544df0cdca454f581
r03iuL <robiulislam1806@gmail.com>
2023-06-12 15:59:10 +0600

    Total student updated

--- src/Pages/Home/TotalStudents/TotalStudents.jsx
+++ src/Pages/Home/TotalStudents/TotalStudents.jsx
@@ -5,7 +5,7 @@ const TotalStudents = () => {
         <div className="hero-content flex-col lg:flex-row-reverse">
           <img
             src="https://img.freepik.com/free-vector/composer-concept-illustration_114360-2844.jpg?w=740&t=st=1686382579~exp=1686383179~hmac=8a1b07ff527d4ba7062c09775d8fb8f2dea0193ddb9e0dcf36ace988ccd134bf"
-            className="max-w-sm rounded-lg shadow-2xl w-full lg:max-w-none lg:w-1/2"
+            className=" rounded-lg shadow-2xl w-full h-full lg:max-w-none lg:w-1/2"
           />
           <div className="px-5 lg:w-1/2">
             <h1 className="pb-4 text-5xl font-bold">Hurry UP!!!</h1>
